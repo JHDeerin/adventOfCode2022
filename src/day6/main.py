2bd0942b9277bed435934885cce4220ef2613168
JHDeerin <2016deerinjo@gmail.com>
2022-12-06 19:50:50 -0500

    day6: Added part 1 test case

--- src/day6/main.py
+++ src/day6/main.py
@@ -1,7 +1,7 @@
 """
-TODO
+As we head to the star groves, the elves hand us a communication device. To communicate, we need to lock on to their signal - a squence of apparently-random characters one ata time. We receive the "datastream buffer"; we need to find the "start-of-packet" marker (which'll be the first sequence of 4 characters that are all different).
 
-PART 1: TODO
+PART 1: How many characters need to be processed before the first start-of-packet marker? (min possible is four)
 
 OUTCOME: TODO
 
@@ -14,29 +14,29 @@ REFLECTIONS: TODO
 from typing import List
 
 
-def part1(input: List[str]):
+def part1(input: str):
     pass
 
 
 def test_first_example():
     with open("test.txt") as file:
-        test_input = file.read().splitlines()
-    assert part1(test_input) == 0
+        test_input = file.read()
+    assert part1(test_input) == 7
 
 
-def part2(input: List[str]):
+def part2(input: str):
     pass
 
 
 def test_second_example():
     with open("test.txt") as file:
-        test_input = file.read().splitlines()
+        test_input = file.read()
     assert part2(test_input) == 0
 
 
 if __name__ == "__main__":
     with open("input.txt") as file:
-        input = file.read().splitlines()
+        input = file.read()
 
     test_first_example()
     result = part1(input)
