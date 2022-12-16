badfb06f75a0ce93c27e8cd190ae0329a5e8487b
JHDeerin <2016deerinjo@gmail.com>
2022-12-15 22:15:27 -0500

    day9: Still debugging 2nd part

--- src/day9/main.py
+++ src/day9/main.py
@@ -9,9 +9,13 @@ PART 1: Assuming we start with the head and tail overlapping, how many distinct 
 
 OUTCOME: Got it right! (6284) (Had a stupid bug where I typed one of the direction keys wrong)
 
-PART 2: TODO
+PART 2: The rope is now 10 knots long; the 1st knot is the head, the 2nd follows the 1st as its head, the 3rd follows the 2nd as its head, etc. Find how many the new "tail" (the 10th knot) visits.
+- Okay, so now, I want to pass in a list of "rope" positions, move the first one, and then update the rest
 
-OUTCOME: TODO
+OUTCOME: Got it wrong the first time with 2639 (too low - didn't use both test cases - but the 2nd test case still passed!?WTF!?)
+-   Okay, the head moved to 7103 positions - so the right answer is between 2640 and 7103...
+-   Okay, printing out a visualization shows me that there are some disconnected islands where the tail somehow "jumped" and skipped a space...hmmm
+    -   I guess my "snapping" logic somehow breaks down in some cases where multiple pieces shift at once? If I change the rope to instead just move 1 in the right direction, does that fix things?
 
 REFLECTIONS: TODO
 """
@@ -48,15 +52,17 @@ def snap_tail(head: Tuple[int, int], tail: Tuple[int, int],) -> Tuple[int, int]:
 
 def play_movements(
     moves: List[Tuple[int, int]],
-    head: Tuple[int, int],
-    tail: Tuple[int, int],
+    knots: List[Tuple[int, int]]
 ) -> Tuple[List[Tuple[int, int]], List[Tuple[int, int]]]:
-    head_positions, tail_positions = [head], [tail]
+    head_positions, tail_positions = [knots[0]], [knots[-1]]
     for move in moves:
-        head = (head[0] + move[0], head[1] + move[1])
-        tail = snap_tail(head, tail)
-        head_positions.append(head)
-        tail_positions.append(tail)
+        for i, knot in enumerate(knots):
+            if i == 0:
+                knots[i] = (knot[0] + move[0], knot[1] + move[1])
+                continue
+            knots[i] = snap_tail(knots[i-1], knot)
+        head_positions.append(knots[0])
+        tail_positions.append(knots[-1])
     return head_positions, tail_positions
 
 
@@ -76,7 +82,7 @@ def test_snap_tail():
 def part1(input: str) -> int:
     head_pos, tail_pos = (0,0), (0,0)
     movements = get_movements(input)
-    head_positions, tail_positions = play_movements(movements, head_pos, tail_pos)
+    head_positions, tail_positions = play_movements(movements, [head_pos, tail_pos])
     return len(set(tail_positions))
 
 
@@ -87,14 +93,43 @@ def test_first_example():
     assert part1(test_input) == 13
 
 
-def part2(input: str):
-    pass
+def print_tail_traversed_paths(tail_positions: List[Tuple[int, int]]):
+    min_x, max_x = min([x[0] for x in tail_positions]), max([x[0] for x in tail_positions])
+    min_y, max_y = min([y[1] for y in tail_positions]), max([y[1] for y in tail_positions])
+
+    unique_tail_pos = set(tail_positions)
+    print(f"{len(unique_tail_pos)} tail pos | x: {min_x} to {max_x} | y: {min_y} to {max_y}")
+    print("=" * 40)
+    for y in range(max_y, min_y-1, -1):
+        chars = []
+        for x in range(min_x, max_x+1):
+            char = "."
+            if (x,y) == (0,0):
+                char= "s"
+            elif (x,y) in unique_tail_pos:
+                char = "#"
+            chars.append(char)
+        print("".join(chars))
+
+
+def part2(input: str) -> int:
+    knots = [(0,0)] * 10
+    movements = get_movements(input)
+    head_positions, tail_positions = play_movements(movements, knots)
+    print_tail_traversed_paths(tail_positions)
+    return len(set(tail_positions))
 
 
 def test_second_example():
     with open("test.txt") as file:
         test_input = file.read()
-    assert part2(test_input) == 0
+    assert part2(test_input) == 1
+
+
+def test_third_example():
+    with open("test2.txt") as file:
+        test_input = file.read()
+    assert part2(test_input) == 36
 
 
 if __name__ == "__main__":
@@ -110,5 +145,7 @@ if __name__ == "__main__":
     assert result == 6284
 
     test_second_example()
+    test_third_example()
+
     result = part2(input)
     print(f"part 2: {result}")
