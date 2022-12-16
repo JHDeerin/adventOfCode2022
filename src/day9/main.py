33434c85c534b54ab8104d6f4229d3db38ff135b
JHDeerin <2016deerinjo@gmail.com>
2022-12-15 21:08:09 -0500

    day9: Added part 1 test

--- src/day9/main.py
+++ src/day9/main.py
@@ -1,7 +1,11 @@
 """
-TODO
+A basic rope simulation: a rope has a head and a tail, and if the head moves far enough away, the tail follows. The head and tail must always be touching (adjacent, diagonal, or overlapping if just "H" on-screen). A 2D grid shows their positions. If the head moves so that it's not touching, the tail follows behind (diagonal ones move so they become adjacent). There are 4 ways the head can move: (U)p, (D)own, (R)ight, (L)eft
 
-PART 1: TODO
+PART 1: Assuming we start with the head and tail overlapping, how many distinct positions does the tail visit as the head moves?
+- Will represent a 2D grid and the head + tail position; then, if the tail is more than 2 away, will "snap" the tail to the right position
+    -   Can tell the tail is far enough away if abs(tail.x - head.x) > 1 or (tail.y - head.y) > 1 (works for diagonals as well)
+    -   To get the unique positions, record all the tail movements x/y tuples (or row/col?), then take the set
+
 
 OUTCOME: TODO
 
@@ -21,7 +25,7 @@ def part1(input: str):
 def test_first_example():
     with open("test.txt") as file:
         test_input = file.read()
-    assert part1(test_input) == 0
+    assert part1(test_input) == 13
 
 
 def part2(input: str):
